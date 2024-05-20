e25e9c19c5405272e06636bcc4b21cb3cb2eb225
llzai <llzaicev@yandex.ru>
2024-05-20 07:07:11 +0300

    added GetOrder implementation

--- TestTask/Services/Implementations/OrderService.cs
+++ TestTask/Services/Implementations/OrderService.cs
@@ -1,13 +1,28 @@
-﻿using TestTask.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
 using TestTask.Services.Interfaces;
 
 namespace TestTask.Services.Implementations
 {
     public class OrderService : IOrderService
     {
+        private readonly ILogger _logger;
+        private readonly ApplicationDbContext _context;
+        public OrderService(ApplicationDbContext context, ILogger<OrderService> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
         public Task<Order> GetOrder()
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"{DateTime.UtcNow.ToLongTimeString()}: GetOrder was called");
+            IQueryable<Order> orders = _context.Orders.Include(o => o.User);
+            return orders
+                .Where(orders => (orders.Quantity > 1))
+                .OrderByDescending(o => o.CreatedAt)
+                .FirstAsync();
         }
 
         public Task<List<Order>> GetOrders()
